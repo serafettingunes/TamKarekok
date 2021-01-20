fa998a7bedd64d2a0d0e151e3a92f56d8dac6789
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-20 17:37:02 +0300

    Add project files.

---
+++ TamKarekok/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TamKarekok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double i = 1; i <= 1000; i++)
+            {
+                if (Math.Sqrt(i).ToString().Length == 2 || Math.Sqrt(i).ToString().Length == 1)
+                {
+                    Console.WriteLine("{0} sayısının karekökü {1}", i, Math.Sqrt(i));
+                }
+
+            }
+        }
+    }
+}
